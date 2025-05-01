5e26c479dbfffe7e7f017d23fa84281f5b2e0ae2
The4codeblocks <72419529+The4codeblocks@users.noreply.github.com>
2025-05-01 02:30:21 -0500

    Initial commit

---
+++ init.lua
@@ -0,0 +1,32 @@
+S = core.get_translator("sundial")
+
+core.register_node("sundial:sundial", {
+	paramtype2 = "facedir",
+	tiles = {"sundial_sundial.png"},
+	drawtype = "mesh",
+	mesh = "sundial.obj",
+	groups = {cracky=1},
+	description = S("Sundial"),
+})
+
+if core.get_modpath("basic_materials") then
+	if core.get_modpath("dye") then
+		core.register_craft({
+			output = "sundial:sundial",
+			recipe = {
+				{"dye:black"},
+				{"basic_materials:plastic_sheet"},
+				{"basic_materials:plastic_sheet"}
+			},
+		})
+	else
+		core.register_craft({
+			output = "sundial:sundial",
+			recipe = {
+				{"basic_materials:plastic_sheet"},
+				{"basic_materials:plastic_sheet"},
+				{"basic_materials:plastic_sheet"}
+			},
+		})
+	end
+end
